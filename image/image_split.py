f02766558b4e10dcb9e646bf6f1c5bf3717f9a5a
seongung33 <seongung33@gmail.com>
2026-05-15 10:31:54 +0900

    아마 끝

---
+++ image/image_split.py
@@ -0,0 +1,68 @@
+from PIL import Image
+import os
+import math
+
+# =========================
+# 1. image_path 에 원하는 이미지 상대경로 복사 후 입력
+# =========================
+image_path = r"image/image_name.gif"  # image/image_name.gif 에서 image_name.gif를 바꾼다.
+base_name = input("파일 기본 이름 입력: ").strip()
+tile_size = 32  # 메타모스트 최적화 사이즈 16 32 64 128 중 선택 크기가 클수록 메타모스트에선 작게 나타남
+output_folder = "output"
+os.makedirs(output_folder, exist_ok=True)
+
+img = Image.open(image_path)
+orig_w, orig_h = img.size
+
+# 나누었을 때 남는 공간을 포함할 수 있도록 올림 처리하여 전체 크기 계산
+cols = math.ceil(orig_w / tile_size)
+rows = math.ceil(orig_h / tile_size)
+new_w, new_h = cols * tile_size, rows * tile_size
+
+# =========================
+# 2. 프레임 처리
+# =========================
+pieces_frames = {(r, c): [] for r in range(rows) for c in range(cols)}
+durations = []
+
+for f in range(img.n_frames):
+    img.seek(f)
+    durations.append(img.info.get("duration", 100))
+    
+    frame_rgba = img.convert("RGBA")
+    
+    full_canvas = Image.new("RGBA", (new_w, new_h), (0, 0, 0, 0))
+    
+
+    full_canvas.paste(frame_rgba, (0, 0))
+    
+    for r in range(rows):
+        for c in range(cols):
+            left, upper = c * tile_size, r * tile_size
+            cropped = full_canvas.crop((left, upper, left + tile_size, upper + tile_size))
+            
+            pieces_frames[(r, c)].append(cropped.copy())
+
+# =========================
+# 3. 저장
+# =========================
+for r in range(rows):
+    for c in range(cols):
+        row_char = chr(65 + r)
+        col_num = c + 1
+        save_path = os.path.join(output_folder, f"{base_name}_{row_char}{col_num}.gif")
+        
+        frames = pieces_frames[(r, c)]
+        
+        frames[0].save(
+            save_path,
+            save_all=True,
+            append_images=frames[1:],
+            duration=durations,
+            loop=0,
+            disposal=2,
+            optimize=False # 최적화 제거
+        )
+        print(f"✅ 저장 완료: {save_path} ({tile_size}x{tile_size})")
+
+print("\n✨ 메타모스트용 최적화 분할 완료!")
---
+++ image_upload.py
@@ -0,0 +1,137 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+from dotenv import load_dotenv
+import os
+from pathlib import Path
+
+# =========================
+# 설정
+# =========================
+env_path = Path(__file__).resolve().parent / ".env"
+
+load_dotenv(dotenv_path=env_path)
+email = os.getenv("ID")
+PASSWORD = os.getenv("PASSWORD")
+MATTERMOST_URL = os.getenv("MATTERMOST_URL")
+
+EMOJI_NAME = "pepe"
+IMAGE_PATH = r"C:\emoji\pepe.png"
+
+# =========================
+# 드라이버 실행
+# =========================
+
+driver = webdriver.Chrome(
+    service=Service(ChromeDriverManager().install())
+)
+
+wait = WebDriverWait(driver, 15)
+
+# =========================
+# 로그인 페이지 이동
+# =========================
+
+driver.get(f"{MATTERMOST_URL}/login")
+## 한번 더 누르기
+time.sleep(2.5)
+url = "mattermost://meeting.ssafy.com/login"
+driver.find_element(
+    By.CSS_SELECTOR,
+    'a[href="https://meeting.ssafy.com/login"]'
+).click()
+
+
+# =========================
+# 로그인
+# =========================
+
+login_input = wait.until(
+    EC.presence_of_element_located((By.CSS_SELECTOR, 'input[id="input_loginId"], input[class="form-control"]'))
+)
+login_input.send_keys(email)
+
+password_input = driver.find_element(By.CSS_SELECTOR, 'input[id="input_password-input"]')
+password_input.send_keys(PASSWORD)
+
+submit_btn = driver.find_element(By.CSS_SELECTOR, "#saveSetting")
+submit_btn.click()
+
+# =========================
+# 로그인 완료 대기
+# =========================
+
+
+# =========================
+# 커스텀 이모지 페이지 이동
+# =========================
+
+wait.until(
+    EC.element_to_be_clickable(
+        (By.ID, "emojiPickerButton")
+    )
+).click()
+# =========================
+# "Add Custom Emoji" 버튼 클릭
+# =========================
+
+wait.until(
+    EC.element_to_be_clickable(
+        (By.XPATH, "//span[text()='Custom Emoji']")
+    )
+).click()
+
+
+
+
+
+time.sleep(3)
+#################
+## 이미지 연속 업로드 시작
+##########
+## 이름 적기
+IMAGE_FOLDER = "output"
+
+files = os.listdir(IMAGE_FOLDER)
+
+for file in files:
+
+    wait.until(
+        EC.element_to_be_clickable(
+            (By.XPATH, "//span[text()='이모티콘 추가']")
+        )
+    ).click()
+
+
+    if not file.endswith((".png", ".jpg", ".jpeg", ".gif")):
+        continue
+
+    emoji_name = os.path.splitext(file)[0]
+
+    image_path = os.path.abspath(
+        os.path.join(IMAGE_FOLDER, file)
+    )
+
+    name_input = wait.until(
+        EC.presence_of_element_located((By.ID, 'name'))
+    )
+    name_input.send_keys(emoji_name)
+
+    file_input = wait.until(
+        EC.presence_of_element_located(
+            (By.ID, 'select-emoji')
+        )
+    )
+    file_input.send_keys(image_path)
+    driver.find_element(
+        By.XPATH,
+        "//span[text()='저장']"
+    ).click()
+
+
+
+##  이미지 선택
